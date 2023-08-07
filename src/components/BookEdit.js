b477a248eec3fda6842216d02573011806674053
Aisha <mirakhalz@gmail.com>
2023-08-07 12:53:15 -0600

    added book edit and save edit feature

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,27 @@
-function BookEdit() {
-    return <div>BookCreate</div>
-}
+import { useState } from "react";
+import BookShow from "./BookShow";
+
+function BookEdit({ book, onSubmit }) {
+
+    const [title, setTitle] = useState(book.title);
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event, id, newTitle) => {
+        event.preventDefault();
+        onSubmit(book.id, title);
+    };
+
+    return (
+    
+    <form onSubmit={handleSubmit} className="book-edit">
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange}/>
+        <button className="button is-primary">Save</button>
+    </form>
+    );
+};
 
 export default BookEdit;
